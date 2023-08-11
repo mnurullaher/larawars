293d6a9d03cea8de37e1763e3491e9920086e642
mnurullaher <mnurullaher@gmail.com>
2023-08-11 23:32:13 +0300

    command test enhanced

--- tests/Feature/MigrateResourcesCommandTest.php
+++ tests/Feature/MigrateResourcesCommandTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Services\PeopleService;
+use App\Services\PlanetService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Artisan;
@@ -10,6 +12,15 @@ use Tests\TestCase;
 class MigrateResourcesCommandTest extends TestCase
 {
     use RefreshDatabase;
+    private PlanetService $planetService;
+    private PeopleService $peopleService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->planetService = new PlanetService();
+        $this->peopleService = new PeopleService();
+    }
 
     public function test_migrate_all_resources_data(): void
     {
@@ -25,5 +36,8 @@ class MigrateResourcesCommandTest extends TestCase
         foreach ($resources as $resource => $count) {
             $this->assertDatabaseCount($resource, $count);
         }
+        $this->assertEquals(true, $this->planetService->getByName('Tatooine')->has_force);
+        $this->assertNotEmpty($this->peopleService->getByName('Han Solo')->starships);
+        $this->assertNotEmpty($this->peopleService->getByName('Anakin Skywalker')->vehicles);
     }
 }
